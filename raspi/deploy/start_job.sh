75419eedd06d8932cab445094ea3219aee193c6f
lichuan <89424619@qq.com>
2024-01-02 12:30:56 +0800

    dev

--- raspi/deploy/start_job.sh
+++ raspi/deploy/start_job.sh
@@ -2,29 +2,46 @@
 
 # 获取当前脚本运行的目录
 currentDir="$( cd "$( dirname "$0" )" && pwd -P )"
-rootDir=$(cd "$script_path" && cd .. && pwd)
-outputDir=$rootDir/output
+rootDir=$(cd "${currentDir}" && cd .. && pwd)
+outputDir="${rootDir}/output"
 
 # 配置文件路径
-configFile=$rootDir/config/raspi_conf.yaml
+configFile="${rootDir}/config/raspi_conf.yaml"
 # 检查并创建日志目录
-logDir="/tmp/job_log"
+logDir=/tmp/job_log
 if [ ! -d "$logDir" ]; then
     mkdir -p "$logDir"
 fi
 
 # 从命令行参数获取参数
-job=$1
-params=$2
+job="$1"
+params="$2"
+
+# 检查 $job 是否为空
+if [ -z "$job" ]; then
+    echo "Error: 'job' parameter is missing. Exiting..."
+    exit 1
+fi
+
+# 检查 $params 是否为空，如果为空则设置默认值 "{}"
+if [ -z "$params" ]; then
+    params="{}"
+fi
+
 # 设置 common_job 的执行命令
-common_job_cmd="${outputDir}/job -job ${job} -params ${params}"
-
+common_job_cmd="${outputDir}/job -job $job -params \"$params\""
+# shellcheck disable=SC2154
+echo the command  is: "$common_job_cmd"
+# 检查 $outputDir/job 文件是否存在
+if [ ! -x "$outputDir"/job ]; then
+    echo Error: "$outputDir"/job not found. Exiting...
+    exit 1
+fi
 # 检查是否存在当前正在运行的进程
-existing_pid=$(pgrep -f "${common_job_cmd}")
-
+existing_pid=$(pgrep -f "$common_job_cmd")
 if [ -n "${existing_pid}" ]; then
-    echo "Existing process found with PID ${existing_pid}. Killing the process..."
-    kill "${existing_pid}"
+    echo "Existing process found with PID $existing_pid. Killing the process..."
+    kill "$existing_pid}】"
     sleep 2  # 等待一段时间确保进程被杀死
 fi
 
@@ -33,8 +50,9 @@ export RASPI_SERVER_CONFIG=$configFile
 
 # 执行 common_job
 # 执行 common_job 并将输出追加到指定文件
-logFile="/tmp/job_log/${job}.log"
+logFile=/tmp/job_log/$job.log
+
 nohup ${common_job_cmd} > "$logFile" 2>&1 &
-echo "$job job started"
-echo "the PID is: $!"
-echo "log file at: $logFile"
+echo ${job} job started
+echo the PID is: $!
+echo log file at: "${logFile}"
